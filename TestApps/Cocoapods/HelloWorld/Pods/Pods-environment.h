a6ff68fe355dcc041e619179f61651e2e877ce9e
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-11-07 16:27:24 +0900

    pod install

---
+++ TestApps/Cocoapods/HelloWorld/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Cordova
+#define COCOAPODS_POD_AVAILABLE_Cordova
+#define COCOAPODS_VERSION_MAJOR_Cordova 3
+#define COCOAPODS_VERSION_MINOR_Cordova 1
+#define COCOAPODS_VERSION_PATCH_Cordova 0
+
+// Cordova/Base
+#define COCOAPODS_POD_AVAILABLE_Cordova_Base
+#define COCOAPODS_VERSION_MAJOR_Cordova_Base 3
+#define COCOAPODS_VERSION_MINOR_Cordova_Base 1
+#define COCOAPODS_VERSION_PATCH_Cordova_Base 0
+
