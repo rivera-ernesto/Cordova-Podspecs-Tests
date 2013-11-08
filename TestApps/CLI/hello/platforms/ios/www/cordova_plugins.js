d832681edddded36dd41d2b375ba9cec93819ae8
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-11-08 14:16:53 +0900

    cordova plugin add org.apache.cordova.console [1/2]

--- TestApps/CLI/hello/platforms/ios/www/cordova_plugins.js
+++ TestApps/CLI/hello/platforms/ios/www/cordova_plugins.js
@@ -437,6 +437,20 @@ module.exports = [
         "clobbers": [
             "window.open"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
     }
 ]
 });
