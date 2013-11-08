5d9e5ef82b82cb2c023bd943175fdef0b6006eaf
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-11-08 14:10:58 +0900

    cordova plugin add org.apache.cordova.media [1/2]

--- TestApps/CLI/hello/platforms/ios/www/cordova_plugins.js
+++ TestApps/CLI/hello/platforms/ios/www/cordova_plugins.js
@@ -297,6 +297,20 @@ module.exports = [
         "clobbers": [
             "navigator.device.capture"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/MediaError.js",
+        "id": "org.apache.cordova.media.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/Media.js",
+        "id": "org.apache.cordova.media.Media",
+        "clobbers": [
+            "window.Media"
+        ]
     }
 ]
 });
